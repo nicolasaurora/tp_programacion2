49c7ee86727bdaa7a06b43f19f14e0ebe3322950
nicolasaurora <nicolas.aurora5@gmail.com>
2026-04-24 12:07:24 -0300

    Se agregaron cards, estilos de cards y logica de cards.

--- js/main.js
+++ js/main.js
@@ -43,6 +43,57 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 
+// Logica juego:
+
+const cards = document.querySelectorAll(".card");
+const contenedor = document.querySelector(".grid");
+
+const arrayCards = Array.from(cards);
+const arrayDesordenado = arrayCards.sort(() => Math.random() - 0.5);
+
+arrayDesordenado.forEach(card => contenedor.appendChild(card));
+
+let primeraCarta = null;
+let segundaCarta = null;
+let bloqueado = false;
+
+cards.forEach(card => {
+
+    card.addEventListener("click", () => {
+
+        if (bloqueado) return;
+        if (card === primeraCarta) return;
+        if (card.classList.contains('flip')) return;
+
+        card.classList.add("flip");
+
+        if (!primeraCarta) {
+            primeraCarta = card;
+        } else {
+            segundaCarta = card;
+            bloqueado = true;
+
+            const img1 = primeraCarta.querySelector("img").src;
+            const img2 = segundaCarta.querySelector("img").src;
+
+            if (img1 === img2) {
+                resetear();
+            } else {
+                setTimeout(() => {
+                    primeraCarta.classList.remove("flip");
+                    segundaCarta.classList.remove("flip");
+                    resetear();
+                }, 1000);
+            }
+        }
+    });
+});
+
+function resetear() {
+    primeraCarta = null;
+    segundaCarta = null;
+    bloqueado = false;
+}
 
 
 
